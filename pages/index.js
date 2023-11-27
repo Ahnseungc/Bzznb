d5b0047ccc7ef24131b50a0f69af7001afd0b0bc
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2023-11-27 21:30:33 +0900

    feat: 메인홈 디자인

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,44 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
-  return <div>h2</div>;
+  return (
+    <div className={styles.MainContainer}>
+      <header>
+        <p className={styles.HomeMainTextW_T}>
+          Welcome to <br />
+          <span className={styles.HomeMainTextW_A}>Weather App!</span>
+        </p>
+      </header>
+      <p className={styles.Introduction}>Choose a city from the list below to check the weather.</p>
+      <div className={styles.MiddleContainer}>
+        <section>
+          <ul className={styles.CountryList}>
+            <li className={styles.Country}>
+              <Link href={'/'} style={{ textDecoration: 'none', color: '#FF0045' }}>
+                Seoul
+              </Link>
+            </li>
+            <li className={styles.Country}>
+              <Link href={'/'} style={{ textDecoration: 'none', color: '#FF0045' }}>
+                Tokyo
+              </Link>
+            </li>
+            <li className={styles.Country}>
+              <Link href={'/'} style={{ textDecoration: 'none', color: '#FF0045' }}>
+                Paris
+              </Link>
+            </li>
+            <li className={styles.Country}>
+              <Link href={'/'} style={{ textDecoration: 'none', color: '#FF0045' }}>
+                London
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <Image className={styles.Earth} src={'/img/img.png'} alt="/" width={430} height={321} />
+    </div>
+  );
 }
